df60fab99c22d24b56585d14951cd77d9596ae55
Ahmet Utlu <ahmet.utlu@basefy.com>
2024-12-22 16:15:31 +0100

    Initial Commit

---
+++ RoutineApp/RoutineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineAppApp.swift
+//  RoutineApp
+//
+//  Created by Ahmet Utlu on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoutineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
